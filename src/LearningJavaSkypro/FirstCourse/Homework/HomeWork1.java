3cdc93e38ce89af54eb580a61b1b6291b32d17b2
Ryabinin <Ryabinin85@spartak.ru>
2022-09-15 20:48:36 +0300

    Cделано ДЗ

--- src/LearningJavaSkypro/FirstCourse/Homework/HomeWork1.java
+++ src/LearningJavaSkypro/FirstCourse/Homework/HomeWork1.java
@@ -2,6 +2,6 @@ package LearningJavaSkypro.FirstCourse.Homework;
 
 public class HomeWork1 {
     public static void main(String[] args) {
-        System.out.println("Привет, мир!");
+        System.out.println("Привет, мир!!!");
     }
 }
